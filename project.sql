aed12f95819d9ed0fc64419b4fdc4a737c6a8466
Alekya Gadiraju <gadirajualekya@gmail.com>
2021-03-11 21:27:51 -0500

    added files

--- project.sql
+++
@@ -1,7 +0,0 @@
-
--- select * from "new_Future"
-
-SELECT "rank_Future"."Restaurant", "rank_Future"."YOY_Sales", "rank_Future"."Sales", "rank_Future"."YOY_Units", "rank_Top250"."Restaurant", "rank_Top250"."YOY_Sales", "rank_Top250"."Sales", "rank_Top250"."YOY_Units"
-FROM "rank_Future"
-JOIN "rank_Top250"
-ON "rank_Future"."Rank"= "rank_Top250"."Rank";
